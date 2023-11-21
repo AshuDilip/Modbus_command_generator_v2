c9aeeeaa0415ca8b5ba94738733b996baa183e2f
AshuDilip <143398557+AshuDilip@users.noreply.github.com>
2023-11-21 14:11:47 +0100

    Add files via upload

--- command_generator.py
+++ command_generator.py
@@ -1,6 +1,7 @@
 import struct
 import binascii
 
+
 def print_spaced_command(modbus_command):
     spaced_command = ' '.join(modbus_command[i:i+2] for i in range(0, len(modbus_command), 2))
     print(spaced_command)
@@ -16,15 +17,22 @@ def calculate_crc(data):
                 crc >>= 1
     return crc.to_bytes(2, byteorder='little')
 
-
-def modbus_command_generator(slave_id, function_code, register_address, value, byteorder, datatype):
+def modbus_command_generator(slave_id, function_code, register_address, value_array, byteorder, datatype):
+    #check if single value is entered or multiple
+    try:
+        x = len(value_array)
+    except:
+        value_array = [value_array]
     # Validate slave ID
     if not (0 <= slave_id <= 255):
         raise ValueError("Slave ID must be in the range 0 to 255.")
 
     # Validate function code
     if function_code not in {6, 16}:
-        raise ValueError("Function code must be 6 (Write Single Register) or 16 (Write Multiple Registers).")
+        raise ValueError("Function code must be 6 (Write Single Register), 16 (Write Multiple Registers).")
+
+    if function_code == 6 and len(value_array)>1:
+        raise ValueError("Function code must 16 when writing multiple registers.")
 
     # Validate register address
     if not (0 <= register_address <= 65535):
@@ -34,40 +42,68 @@ def modbus_command_generator(slave_id, function_code, register_address, value, b
     if byteorder not in {'HL', 'LH'}:
         raise ValueError("Byte order must be 'HL' (High to Low) or 'LH' (Low to High).")
 
+    #Pack struct according to byteorder
+    if byteorder=='HL':
+        pack_sign='>'
+    else:
+        pack_sign='<'
+
     # Validate data type
     valid_data_types = {'S', 'U'}
-    if not (datatype[0] in valid_data_types and datatype[1:3].isdigit() and datatype[-1] == 'I'):
+    if not (datatype[0] in valid_data_types and datatype[1:3].isdigit() and (datatype[-1] == 'I' or datatype[-1] == 'F')):
         raise ValueError("Invalid data type.")
-
-    if datatype[0] == 'U' and value < 0:
-        raise ValueError("Unsigned data type cannot have a negative value.")
-
-    if datatype[0] == 'S' and not (-2 ** (int(datatype[1:3]) - 1) <= value <= 2 ** (int(datatype[1:3]) - 1) - 1):
-        raise ValueError(f"Value out of range for signed {datatype} data type.")
-
-    if datatype[0] == 'U' and not (0 <= value <= 2 ** (int(datatype[1:3])) - 1):
-        raise ValueError(f"Value out of range for unsigned {datatype} data type.")
 
     # Convert register address and value to bytes
     register_address_bytes = register_address.to_bytes(2, byteorder='big')
 
-    if isinstance(value, int):
-        if datatype[0] == 'S':
-            value_bytes = struct.pack('>h', value)  # Signed 16-bit integer
-        elif datatype[0] == 'U':
-            value_bytes = struct.pack('>H', value)  # Unsigned 16-bit integer
-        else:
-            raise ValueError("Invalid datatype")
-    else:
-        raise ValueError("Invalid value type or datatype mismatch.")
-
-    # Combine all parts to form the message
     message = bytearray([
         slave_id,
         function_code,
         *register_address_bytes,
-        *value_bytes
     ])
+
+    for value in value_array:
+
+        if datatype[0] == 'U' and value < 0:
+            raise ValueError("Unsigned data type cannot have a negative value.")
+
+        if datatype[3] == 'I':
+            if datatype[0] == 'S' and not (-2 ** (int(datatype[1:3]) - 1) <= value <= 2 ** (int(datatype[1:3]) - 1) - 1):
+                raise ValueError(f"Value out of range for signed {datatype} data type.")
+
+            if datatype[0] == 'U' and not (0 <= value <= 2 ** (int(datatype[1:3])) - 1):
+                raise ValueError(f"Value out of range for unsigned {datatype} data type.")
+
+        if datatype[3]=='I':
+            if not isinstance(value,int):
+                raise ValueError("Invalid datatype")
+            if [datatype[1],datatype[2]] == ['1','6']:
+                if datatype[0] == 'S':
+                    value_bytes = struct.pack(pack_sign+'h', value)  # Signed 16-bit integer
+                elif datatype[0] == 'U':
+                    value_bytes = struct.pack(pack_sign+'H', value)  # Unsigned 16-bit integer
+                else:
+                    raise ValueError("Invalid datatype")
+            elif [datatype[1],datatype[2]] == ['3','2']:
+                if datatype[0] == 'S':
+                    value_bytes = struct.pack(pack_sign+'i', value)  # Signed 16-bit integer
+                elif datatype[0] == 'U':
+                    value_bytes = struct.pack(pack_sign+'I', value)  # Unsigned 16-bit integer
+                else:
+                    raise ValueError("Datatype and bytesize mismatch")
+            else:
+                raise ValueError("Invalid datatype")
+
+        elif datatype[3]=='F':
+            if [datatype[1], datatype[2]] == ['3', '2']:
+                value_bytes = struct.pack(pack_sign+'f', value)  # Float32
+            else:
+                raise ValueError("Datatype and bytesize mismatch")
+        else:
+            raise ValueError("Invalid datatype")
+
+        # Combine all parts to form the message
+        message.extend(bytearray([*value_bytes]))
 
     # Calculate CRC and append it to the message
     crc = calculate_crc(message)
