1ec12906ab5edd046c7666be46e2c1cb01247c8c
AshuDilip <143398557+AshuDilip@users.noreply.github.com>
2023-11-21 16:10:28 +0100

    Add files via upload

--- test_for_goodwe_commands.py
+++ test_for_goodwe_commands.py
@@ -11,9 +11,23 @@ commands_required=[
     [1,6,47517,0,'HL','S16I'],
 ]
 
+commands_required_reading=[
+    [1,3,35187,1]]
+
+
 for i in commands_required:
     try:
         modbus_command = cgen.modbus_command_generator(*i)
         cgen.print_spaced_command(modbus_command)
     except ValueError as e:
         print(f"Error: {e}")
+
+print('\n')
+
+for i in commands_required_reading:
+    try:
+        modbus_command = cgen.modbus_reading_command_generator(*i)
+        cgen.print_spaced_command(modbus_command)
+    except ValueError as e:
+        print(f"Error: {e}")
+
