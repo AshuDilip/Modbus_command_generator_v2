f7af64b451e0dacdc36a47083c59bbac667e87eb
AshuDilip <143398557+AshuDilip@users.noreply.github.com>
2023-11-21 16:06:32 +0100

    Add files via upload

--- command_generator.py
+++ command_generator.py
@@ -49,8 +49,8 @@ def modbus_command_generator(slave_id, function_code, register_address, value_ar
         pack_sign='<'
 
     # Validate data type
-    valid_data_types = {'S', 'U'}
-    if not (datatype[0] in valid_data_types and datatype[1:3].isdigit() and (datatype[-1] == 'I' or datatype[-1] == 'F')):
+    valid_data_types = {'S16I', 'U16I', 'S32I', 'U32I', 'S32F', 'U32F', 'S64F', 'U64F', 'S64I', 'U64I'}
+    if datatype not in valid_data_types:
         raise ValueError("Invalid data type.")
 
     # Convert register address and value to bytes
@@ -69,26 +69,30 @@ def modbus_command_generator(slave_id, function_code, register_address, value_ar
 
         if datatype[3] == 'I':
             if datatype[0] == 'S' and not (-2 ** (int(datatype[1:3]) - 1) <= value <= 2 ** (int(datatype[1:3]) - 1) - 1):
-                raise ValueError(f"Value out of range for signed {datatype} data type.")
+                 raise ValueError(f"Value out of range for signed {datatype} data type.")
 
             if datatype[0] == 'U' and not (0 <= value <= 2 ** (int(datatype[1:3])) - 1):
-                raise ValueError(f"Value out of range for unsigned {datatype} data type.")
+                 raise ValueError(f"Value out of range for unsigned {datatype} data type.")
 
-        if datatype[3]=='I':
-            if not isinstance(value,int):
-                raise ValueError("Invalid datatype")
             if [datatype[1],datatype[2]] == ['1','6']:
                 if datatype[0] == 'S':
-                    value_bytes = struct.pack(pack_sign+'h', value)  # Signed 16-bit integer
+                    value_bytes = struct.pack(pack_sign+'h', value)  # Signed 16-bit integer (short)
                 elif datatype[0] == 'U':
-                    value_bytes = struct.pack(pack_sign+'H', value)  # Unsigned 16-bit integer
+                    value_bytes = struct.pack(pack_sign+'H', value)  # Unsigned 16-bit integer (unsigned short)
                 else:
-                    raise ValueError("Invalid datatype")
+                    raise ValueError("Datatype and bytesize mismatch")
             elif [datatype[1],datatype[2]] == ['3','2']:
                 if datatype[0] == 'S':
-                    value_bytes = struct.pack(pack_sign+'i', value)  # Signed 16-bit integer
+                    value_bytes = struct.pack(pack_sign+'i', value)  # Signed 32-bit integer (int)
                 elif datatype[0] == 'U':
-                    value_bytes = struct.pack(pack_sign+'I', value)  # Unsigned 16-bit integer
+                    value_bytes = struct.pack(pack_sign+'I', value)  # Unsigned 32-bit integer (unsigned int)
+                else:
+                    raise ValueError("Datatype and bytesize mismatch")
+            elif [datatype[1],datatype[2]] == ['6','4']:
+                if datatype[0] == 'S':
+                    value_bytes = struct.pack(pack_sign+'q', value)  # Signed 64-bit integer (long long)
+                elif datatype[0] == 'U':
+                    value_bytes = struct.pack(pack_sign+'Q', value)  # Unsigned 64-bit integer (unsigned long long)
                 else:
                     raise ValueError("Datatype and bytesize mismatch")
             else:
@@ -96,7 +100,9 @@ def modbus_command_generator(slave_id, function_code, register_address, value_ar
 
         elif datatype[3]=='F':
             if [datatype[1], datatype[2]] == ['3', '2']:
-                value_bytes = struct.pack(pack_sign+'f', value)  # Float32
+                value_bytes = struct.pack(pack_sign+'f', value)  # Float32 (float)
+            elif [datatype[1], datatype[2]] == ['6', '4']:
+                value_bytes = struct.pack(pack_sign+'d', value)  # Float64 (double)
             else:
                 raise ValueError("Datatype and bytesize mismatch")
         else:
@@ -113,4 +119,41 @@ def modbus_command_generator(slave_id, function_code, register_address, value_ar
     hex_message = binascii.hexlify(message).decode('utf-8').upper()
 
     return hex_message
+
+def modbus_reading_command_generator(slave_id, function_code, register_address, number_of_regs):
+    # Validate slave ID
+    if not (0 <= slave_id <= 255):
+        raise ValueError("Slave ID must be in the range 0 to 255.")
+
+    # Validate function code
+    if function_code not in {3, 4}:
+        raise ValueError("Function code must be 3 (Read Holding Register), 4 (Read Input Registers).")
+
+    # Validate register address
+    if not (0 <= register_address <= 65535):
+        raise ValueError("Register address must be in the range 0 to 65535.")
+
+    # Convert register address and value to bytes
+    register_address_bytes = register_address.to_bytes(2, byteorder='big')
+
+    number_of_regs_bytes=number_of_regs.to_bytes(2, byteorder='big')
+
+    message = bytearray([
+        slave_id,
+        function_code,
+        *register_address_bytes,
+        *number_of_regs_bytes,
+    ])
+
+    # Calculate CRC and append it to the message
+    crc = calculate_crc(message)
+    message += crc
+
+    # Convert the entire message to hexadecimal
+    hex_message = binascii.hexlify(message).decode('utf-8').upper()
+
+    return hex_message
+
+
+
 
